7d0ef735708c47acb48f6ad71a4033d2379e8847
Prashant <prashantsai@hotmail.com>
2018-08-19 00:04:17 -0700

    some renaming, new public var assignments in scene.unity

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,18 +3,14 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    public GameObject playerObject;
-    public GameObject player2Object;
-    private Player player;
-    private Player player2;
-    private Player current;
+    public Player player1;
+    public Player player2;
+
+    private Player currentPlayer;
 
     // Use this for initialization
     void Start()
     {
-        player = playerObject.GetComponent<Player>();
-        player2 = player2Object.GetComponent<Player>();
-
         TurnManager.instance.OnTurnStart += UpdateCurrentPlayer;
     }
 
@@ -27,7 +23,7 @@ public class InputManager : MonoBehaviour
     public void HandleInputChange(string input)
     {
         string[] commands = input.Split(' ');
-        Debug.Log(current.playerId + ": " + commands[0]);
+        Debug.Log(currentPlayer.playerId + ": " + commands[0]);
 
         int workerId = Int32.Parse(commands[1]);
 
@@ -35,9 +31,9 @@ public class InputManager : MonoBehaviour
 
         if (commands[0].ToLower() == "move")
         {
-            if (current.CanMove(workerId, coordinate))
+            if (currentPlayer.CanMove(workerId, coordinate))
             {
-                current.MoveBuilder(workerId, coordinate);
+                currentPlayer.MoveBuilder(workerId, coordinate);
 
             }
             else
@@ -47,9 +43,9 @@ public class InputManager : MonoBehaviour
         }
         else if (commands[0].ToLower() == "build")
         {
-            if (current.CanBuild(workerId, coordinate))
+            if (currentPlayer.CanBuild(workerId, coordinate))
             {
-                current.PlaceBuilding(workerId, coordinate);
+                currentPlayer.PlaceBuilding(workerId, coordinate);
             }
             else
             {
@@ -58,9 +54,9 @@ public class InputManager : MonoBehaviour
         }
         else if (commands[0].ToLower() == "place")
         {
-            if (current.CanPlace(workerId, coordinate))
+            if (currentPlayer.CanPlace(workerId, coordinate))
             {
-                current.PlaceBuilder(workerId, coordinate);
+                currentPlayer.PlaceBuilder(workerId, coordinate);
             }
             else
             {
@@ -73,11 +69,11 @@ public class InputManager : MonoBehaviour
     {
         if (args.player == 1)
         {
-            current = player;
+            currentPlayer = player1;
         }
         else if (args.player == 2)
         {
-            current = player2;
+            currentPlayer = player2;
         }
         else
         {
