50bd4da66be9603b79b085b33d216b67873799fa
prashant-naidu <naidu.s.prashant@gmail.com>
2018-08-25 22:08:52 -0700

    InputManager now listens to MapTileButton clicks

--- Assets/Scripts/MapGrid.cs
+++ Assets/Scripts/MapGrid.cs
@@ -1,11 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class MapGrid : MonoBehaviour {
 
     public GameObject MapTileButtonPrefabReference = null;
     public Dictionary<Coordinate, MapTileButton> MapTiles = new Dictionary<Coordinate, MapTileButton>();
+
+    #region Event Definition
+
+    public class MapGridEventArgs : EventArgs
+    {
+        private Coordinate m_Coordinate = null;
+        public Coordinate coordinate
+        {
+            get { return m_Coordinate; }
+            set { m_Coordinate = value; }
+        }
+
+        public MapGridEventArgs(Coordinate coordinate)
+        {
+            m_Coordinate = coordinate;
+        }
+
+    }
+
+    public event EventHandler<MapGridEventArgs> OnMapTileButtonPressed;
+    private void RaiseOnMapTileButtonPressed(Coordinate coordinate)
+    {
+        if (coordinate != null && OnMapTileButtonPressed != null)
+        {
+            OnMapTileButtonPressed(this, new MapGridEventArgs(coordinate));
+        }
+    }
+
+    #endregion
 
     public void AddMapTile(Coordinate coordinate)
     {
@@ -19,11 +50,19 @@ public class MapGrid : MonoBehaviour {
         // Set prefab instance's parent to this transform
         mapTile.transform.SetParent(transform);
 
+        // Set Button On Click Listener
+        mapTile.GetComponent<Button>().onClick.AddListener(delegate { MapTileButtonOnClick(mapTile.GetComponent<MapTileButton>().coordinate); });
+
         // Adjust rotation and scale
         RectTransform mapTileRectTransform = mapTile.GetComponent<RectTransform>();
         mapTileRectTransform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
         mapTileRectTransform.localScale = new Vector3(1f, 1f, 1f);
 
         MapTiles.Add(coordinate, mapTile.GetComponent<MapTileButton>());
+    }
+
+    private void MapTileButtonOnClick(Coordinate coordinate)
+    {
+        RaiseOnMapTileButtonPressed(coordinate);
     }
 }
